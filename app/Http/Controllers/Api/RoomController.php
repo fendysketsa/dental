7a87053565699dd3ac49ad5b6208c5e47c1f2ac9
fendysketsa <fendysketsa@gmail.com>
2021-03-08 22:34:00 +0700

    update typo

--- app/Http/Controllers/Api/RoomController.php
+++ app/Http/Controllers/Api/RoomController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Models\RoomsModel;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Http\Resources\SliderCollection;
+use App\Http\Resources\RoomsCollection;
 
 class RoomController extends Controller
 {
